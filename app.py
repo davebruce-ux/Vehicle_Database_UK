07f67755b8626b80ab12f73f169eece0d44dcce0
davebruce-ux <dave_bruce@outlook.com>
2026-06-09 15:39:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ st.markdown("""
 
 # --- HEADER ---
 # Ensure your logo file is uploaded to the root of your repo
-st.image("641c5719-41d0-40ec-adc9-eb3a3e763903.png", width=200)
-st.caption("FIELD LOOKUP // VEHICLE LIBRARY")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("641c5719-41d0-40ec-adc9-eb3a3e763903.png", width=200)
+    st.caption("VEHICLE LIBRARY")
 
 # --- LOAD DATA ---
 @st.cache_data(ttl=60)
